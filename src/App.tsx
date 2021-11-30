c9f48ddaf488c72b01129c2fe1590edb986bae06
elliot0413 <elliot0413@naver.com>
2021-11-30 21:56:18 +0900

    App Bar 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
-import { useState } from "react";
-import Button from "./Button";
-import Calculator from "./pages/Test/components/Calculator";
-import Test from "./pages/Test";
+import { Toolbar, AppBar, Grid, Box } from "@mui/material";
 
-const App = () => {
-  const [latestName, setLatestName] = useState("");
-  const updateLatestName = (name: string) => {
-    setLatestName(name);
-  };
-
+const App = (): JSX.Element => {
   return (
-    <>
-      <Button name="hello " callback={updateLatestName} />
-      <Button name="world" callback={updateLatestName} />
-      <Button name="wnextop" callback={updateLatestName} />
-      <article>{latestName} is clicked!</article>
+    <main>
+      <AppBar position="fixed">
+        <Toolbar></Toolbar>
+      </AppBar>
 
-      <Calculator />
+      <Toolbar />
 
-      <Test />
-    </>
+      <AppBar position="fixed" sx={{ top: "auto", bottom: 0 }}>
+        <Toolbar>
+          <Grid container>
+            <Grid item xs style={{ backgroundColor: "#ff2fff" }}>
+              <Box display="flex" justifyContent="center">
+                홈
+              </Box>
+            </Grid>
+            <Grid item xs style={{ backgroundColor: "#ff2fff" }}>
+              <Box display="flex" justifyContent="center">
+                동네생활
+              </Box>
+            </Grid>
+            <Grid item xs style={{ backgroundColor: "#ff2fff" }}>
+              <Box display="flex" justifyContent="center">
+                내 근처
+              </Box>
+            </Grid>
+            <Grid item xs style={{ backgroundColor: "#ff2fff" }}>
+              <Box display="flex" justifyContent="center">
+                채팅
+              </Box>
+            </Grid>
+            <Grid item xs style={{ backgroundColor: "#ff2fff" }}>
+              <Box display="flex" justifyContent="center">
+                나의 당근
+              </Box>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </main>
   );
 };
 
--- src/pages/Test/index.tsx
+++ src/pages/Test/index.tsx
@@ -4,15 +4,25 @@ const Test = (): JSX.Element => {
   return (
     <Container style={{ backgroundColor: "#022fff" }}>
       <Grid container style={{ backgroundColor: "#ff2fff" }}>
-        <Grid item xs={12}>
+        <Grid item xs={12} md={6}>
+          xs-12
+        </Grid>
+        <Grid item xs={12} md={6}>
           xs-12
         </Grid>
       </Grid>
+
       <Grid container style={{ backgroundColor: "#ff9fff" }}>
-        <Grid item xs={6} style={{ backgroundColor: "#ffff2f" }}>
+        <Grid item xs={6} md={3} style={{ backgroundColor: "#ffff2f" }}>
           xs-6
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={6} md={3} style={{ backgroundColor: "#ffff2f" }}>
+          xs-6
+        </Grid>
+        <Grid item xs={6} md={3} style={{ backgroundColor: "#ffff2f" }}>
+          xs-6
+        </Grid>
+        <Grid item xs={6} md={3} style={{ backgroundColor: "#ffff2f" }}>
           xs-6
         </Grid>
       </Grid>
