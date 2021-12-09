b413724498339fcc556609d39f97b0e51872089e
elliot0413 <elliot0413@naver.com>
2021-12-09 20:28:50 +0900

    LIST(data)

---
+++ src/pages/Home/data.ts
@@ -0,0 +1,102 @@
+type TradeItem = {
+  id: string;
+  imageUrl: string;
+  title: string;
+  location: string;
+  createdAt: Date;
+  updatedAt?: Date;
+  price: number;
+};
+
+export const tradeItems: TradeItem[] = [
+  {
+    id: "1",
+    imageUrl: "https://t1.daumcdn.net/cfile/tistory/9964504D5D07495A09",
+    title: "title1",
+    location: "location1",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    price: 1,
+  },
+  {
+    id: "2",
+    imageUrl: "https://t1.daumcdn.net/cfile/tistory/9964504D5D07495A09",
+    title: "title2",
+    location: "location2",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    price: 2,
+  },
+  {
+    id: "3",
+    imageUrl: "https://t1.daumcdn.net/cfile/tistory/9964504D5D07495A09",
+    title: "title3",
+    location: "location3",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    price: 3,
+  },
+  {
+    id: "4",
+    imageUrl: "https://t1.daumcdn.net/cfile/tistory/9964504D5D07495A09",
+    title: "title4",
+    location: "location4",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    price: 4,
+  },
+  {
+    id: "5",
+    imageUrl: "https://t1.daumcdn.net/cfile/tistory/9964504D5D07495A09",
+    title: "title5",
+    location: "location5",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    price: 5,
+  },
+  {
+    id: "6",
+    imageUrl: "https://t1.daumcdn.net/cfile/tistory/9964504D5D07495A09",
+    title: "title6",
+    location: "location6",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    price: 6,
+  },
+  {
+    id: "7",
+    imageUrl: "https://t1.daumcdn.net/cfile/tistory/9964504D5D07495A09",
+    title: "title7",
+    location: "location7",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    price: 7,
+  },
+  {
+    id: "8",
+    imageUrl: "https://t1.daumcdn.net/cfile/tistory/9964504D5D07495A09",
+    title: "title8",
+    location: "location8",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    price: 8,
+  },
+  {
+    id: "9",
+    imageUrl: "https://t1.daumcdn.net/cfile/tistory/9964504D5D07495A09",
+    title: "title9",
+    location: "location9",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    price: 9,
+  },
+  {
+    id: "10",
+    imageUrl: "https://t1.daumcdn.net/cfile/tistory/9964504D5D07495A09",
+    title: "title10",
+    location: "location10",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    price: 10,
+  },
+];
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,150 @@
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import { Avatar } from "@mui/material";
+import { ListItemAvatar, ListItemText } from "@mui/material";
+
+const ImageTheme = {
+  width: "150px",
+  height: "150px",
+};
+
+const Home = (): JSX.Element => {
+  return (
+    <Box>
+      <List>
+        <ListItem>
+          <ListItemButton>
+            <ListItemAvatar>
+              <Avatar
+                variant="rounded"
+                src="https://t1.daumcdn.net/cfile/tistory/9964504D5D07495A09"
+                sx={ImageTheme}
+              />
+            </ListItemAvatar>
+            <ListItemText primary="제목글" secondary="hello" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihi</ListItemButton>
+        </ListItem>
+
+        <ListItem>
+          <ListItemButton> hihasdfasdf i</ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
+};
+
+export default Home;
